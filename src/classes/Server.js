d0852577e98d352aaf8c7883673daa70df183ab9
Jean Carlos Ademir Garcia Palomino <i201623068@cibertec.edu.pe>
2020-02-07 19:56:35 -0500

    Agregue el endpoint inicial

--- src/classes/Server.js
+++ src/classes/Server.js
@@ -34,6 +34,9 @@ class Server {
     }
     cargarRutas() {
         // Asignando todas las rutas de pabellon_router al servidor
+        this.app.get('/',(req,res)=>{
+            res.status(200).send("La API funciona 😆😆");
+        })
         this.app.use('/', pabellon_router, ambiente_router, reserva_router, usuario_router);
     }
 
