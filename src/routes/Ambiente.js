97b6517719a778f681f4cef12a679f036b4a7381
Jean Carlos Ademir Garcia Palomino <i201623068@cibertec.edu.pe>
2020-02-09 11:23:10 -0500

    Cambio en el post de ambientes

--- src/routes/Ambiente.js
+++ src/routes/Ambiente.js
@@ -4,7 +4,7 @@ const ambiente_router = express.Router();
 const { middleware } = require('./../utils/utils');
 
 ambiente_router.get('/ambiente', Ambientes.getAmbienteWithPabellon);
-ambiente_router.post('/ambiente', middleware, Ambientes.postAmbiente)
+ambiente_router.post('/ambiente', Ambientes.postAmbiente)
 ambiente_router.put('/ambiente/:id_ambiente', middleware, Ambientes.putAmbiente)
 
 module.exports = {
